0fbb8ddbe3b4e5bdf4e4616195530bbc3e267023
Jonas Bengtsson <jonas.b@gmail.com>
2013-01-24 21:31:28 +0100

    uno -> tetra, still no rotation

--- VGATetris.ino
+++ VGATetris.ino
@@ -44,10 +44,49 @@ const byte block_colors[] = {
   0b11000010,
   0b00110001,
   0b00000011,
-  0b11100000,
+  0b10000000,
   0b00100011,
 };
-byte current_block = BLOCK_NONE;
+const byte BLOCK_I_STRUCTURE[4][4] = {
+ 0, 0, 0, 0,
+ 1, 1, 1, 1,
+ 0, 0, 0, 0,
+ 0, 0, 0, 0,
+};
+const byte BLOCK_O_STRUCTURE[2][2] = {
+ 1, 1,
+ 1, 1,
+};
+const byte BLOCK_T_STRUCTURE[3][3] = {
+ 0, 1, 0,
+ 1, 1, 1,
+ 0, 0, 0,
+};
+const byte BLOCK_S_STRUCTURE[3][3] = {
+ 0, 1, 1,
+ 1, 1, 0,
+ 0, 0, 0,
+};
+const byte BLOCK_Z_STRUCTURE[3][3] = {
+ 1, 1, 0,
+ 0, 1, 1,
+ 0, 0, 0,
+};
+const byte BLOCK_J_STRUCTURE[3][3] = {
+ 1, 0, 0,
+ 1, 1, 1,
+ 0, 0, 0,
+};
+const byte BLOCK_L_STRUCTURE[3][3] = {
+ 0, 0, 1,
+ 1, 1, 1,
+ 0, 0, 0,
+};
+
+
+byte current_block_type = BLOCK_NONE;
+const byte *current_block_structure = 0;
+byte current_block_size;
 byte current_col = COLUMN_COUNT / 1;
 byte current_row = 0;
 
@@ -101,7 +140,8 @@ void setup() {
   // setup playfield
   memset(playfield, BLOCK_NONE, ROW_COUNT * COLUMN_COUNT);
   draw_field();
-  current_block = get_next_block();
+  select_next_block();
+  //debug_draw_all_colors(0, 0, Y_PIXELS);
   // control
   pinMode(11, INPUT);
   digitalWrite(11, HIGH);
@@ -109,6 +149,15 @@ void setup() {
   digitalWrite(12, HIGH);
   pinMode(8, INPUT);
   digitalWrite(8, HIGH);
+}
+
+void debug_draw_all_colors(byte start_x, byte start_y, byte height) {
+  for (byte c = 0; c < 64; c++) {
+    byte x = start_x + c / height;
+    byte y = start_y + c % height;
+    byte color = ((c << 2) & 0b11110000) | c & 0b00000011;
+    bitmap[y][x] = color;
+  }
 }
 
 void loop() {
@@ -146,12 +195,13 @@ void loop() {
       time = 0;
       if (!move_block(0, 1)) {
         // Block is stuck
+        add_block_to_playfield();
         collapse_full_rows();
 
         current_row = 0;
         current_col = COLUMN_COUNT / 2;
-        current_block = get_next_block();
-        update_block(current_col, current_row, current_block);
+        select_next_block();
+        update_block(current_col, current_row, current_block_type);
       }
     }
   }
@@ -165,7 +215,7 @@ void read_key(int pin, byte &key_consecutive_press) {
   }
 }
 
-byte get_next_block() {
+void select_next_block() {
   ++block_bag_index;
   if (block_bag_index == BLOCK_BAG_COUNT) {
     block_bag_index = 0;
@@ -177,22 +227,72 @@ byte get_next_block() {
       block_bag[i] = t;
     }
   }
-  return block_bag[block_bag_index];
+  current_block_type = block_bag[block_bag_index];
+  switch (current_block_type) {
+  default:
+  case BLOCK_I:
+    current_block_structure = (byte *)BLOCK_I_STRUCTURE;
+    current_block_size = 4;
+    break;
+  case BLOCK_O:
+    current_block_structure = (byte *)BLOCK_O_STRUCTURE;
+    current_block_size = 2;
+    break;
+  case BLOCK_T:
+    current_block_structure = (byte *)BLOCK_T_STRUCTURE;
+    current_block_size = 3;
+    break;
+  case BLOCK_S:
+    current_block_structure = (byte *)BLOCK_S_STRUCTURE;
+    current_block_size = 3;
+    break;
+  case BLOCK_Z:
+    current_block_structure = (byte *)BLOCK_Z_STRUCTURE;
+    current_block_size = 3;
+    break;
+  case BLOCK_J:
+    current_block_structure = (byte *)BLOCK_J_STRUCTURE;
+    current_block_size = 3;
+    break;
+  case BLOCK_L:
+    current_block_structure = (byte *)BLOCK_L_STRUCTURE;
+    current_block_size = 3;
+    break;
+  }
+}
+
+void add_block_to_playfield() {
+  for (byte block_y = 0; block_y < current_block_size; block_y++) {
+    for (byte block_x = 0; block_x < current_block_size; block_x++) {
+      if (current_block_structure[block_y * current_block_size + block_x] != 0) {
+        playfield[current_row + block_y][current_col + block_x] = current_block_type;
+      }
+    }
+  }
 }
 
 boolean move_block(byte dx, byte dy) {
   byte next_col = current_col + dx;
   byte next_row = current_row + dy;
-  if (next_col < 0 || next_col >= COLUMN_COUNT || next_row >= ROW_COUNT) {
-    return false;
+  for (byte block_y = 0; block_y < current_block_size; block_y++) {
+    for (byte block_x = 0; block_x < current_block_size; block_x++) {
+      if (current_block_structure[block_y * current_block_size + block_x] != 0) {
+        byte x = next_col + block_x;
+        byte y = next_row + block_y;
+        if (x < 0 || x >= COLUMN_COUNT || y >= ROW_COUNT) {
+          return false;
+        }
+        if (playfield[y][x] != BLOCK_NONE) {
+          return false;
+        }
+      }
+    }
   }
-  if (playfield[next_row][next_col] != BLOCK_NONE) {
-    return false;
-  }
+
   update_block(current_col, current_row, BLOCK_NONE);
   current_col = next_col;
   current_row = next_row;
-  update_block(current_col, current_row, current_block);
+  update_block(current_col, current_row, current_block_type);
   return true;
 }
 
@@ -234,7 +334,13 @@ void draw_field() {
 }
 
 void update_block(byte col, byte row, byte block) {
-  playfield[row][col] = block;
-  bitmap[playfield_offset_y + row][playfield_offset_x + col] = block_colors[block];
+  byte color = block_colors[block];
+  for (byte block_y = 0; block_y < current_block_size; block_y++) {
+    for (byte block_x = 0; block_x < current_block_size; block_x++) {
+      if (current_block_structure[block_y * current_block_size + block_x] != 0) {
+        bitmap[playfield_offset_y + row + block_y][playfield_offset_x + col + block_x] = color;
+      }
+    }
+  }
 }
 
